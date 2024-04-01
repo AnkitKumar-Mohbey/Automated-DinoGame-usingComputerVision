d260f178800ab5070c751c7e29d959fbc864cab3
I ANKIT KUMAR <91773999+AnkitKumar-Mohbey@users.noreply.github.com>
2024-04-01 21:00:45 +0530

    Added initial setup and functionality to capture screen shots
    Today's commit includes the following changes:
    - Imported OpenCV library (cv2) for image processing
    - Imported NumPy library (np) for numerical operations
    - Imported cvzone library for accessing the FPS class
    - Imported mss library for capturing screen shots
    - Started capturing screen shots of the current screen
    
    This commit lays the foundation for further development, allowing for the capture and processing of screen shots.

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
+import cv2
+import cvzone
+import numpy as np
+import pyautogui
+form cvzone.FPS import FPS
+from mss import mss
+
+def capture_screen_region_opencv(x, y, desired_width, desired_height):
+  screenshot = pyautogui.screenshot(region=(x, y, desired_width, desired_height))
+  screenshot = np.array(screenshot)
+  screenshot = cv2.cvtColor(screenshot, cv2.COLOR_RGB2BGR)
+  return screenshot
 
